01178c0c8b74a6c7f5dec5bcd4a2836469d0ea4a
jayeuse <ceejay.feruelo@gmail.com>
2024-10-19 13:37:24 +0800

    Add test.js

---
+++ Lesson1.js
@@ -0,0 +1,19 @@
+let myVariable;
+
+myVariable = 123;
+
+console.log(myVariable); // This should print 123
+
+myVariable = 456;
+
+console.log(myVariable); // This should print 456
+
+const MYVARIABLE = 123;
+
+const PI = 3.14;
+
+let myString = 'hello';
+let myString2 = "hello";
+
+console.log(myString); // This should print 'hello'
+console.log(myString2); // This should print 'hello'
